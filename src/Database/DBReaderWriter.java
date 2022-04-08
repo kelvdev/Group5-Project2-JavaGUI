d9eb47be7b3f03ebac77c045a224ec45d5a59327
kelvdev <89535133+kelvdev@users.noreply.github.com>
2022-04-08 00:08:55 -0400

    update 2
    made some UI changes and created a patients screen

---
+++ src/Database/DBReaderWriter.java
@@ -0,0 +1,10 @@
+package Database;// Helper class to read and write to the database with ease
+
+// TODO: implement Database.DBReaderWriter methods to do specific tasks such as addPatient() or editPatient();
+public class DBReaderWriter {
+
+    public DBReaderWriter(){
+
+    }
+
+}
